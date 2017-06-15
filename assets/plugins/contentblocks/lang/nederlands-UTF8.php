5ffcce5689eb6b007da4b255c5d775d6171cec6a
sunhaim <nm@perm-tools.ru>
2017-06-15 14:20:42 +0500

    Create nederlands-UTF8.php, #1

---
+++ assets/plugins/contentblocks/lang/nederlands-UTF8.php
@@ -0,0 +1,14 @@
+<?php
+	return [
+		'Add block' => 'Blok toevoegen',
+		'Choose' => 'Selecteer',
+		'Insert element' => 'Element toevoegen',
+		'Delete element' => 'Element verwijderen',
+		'Select type of block' => 'Selecteer type blok',
+		'Insert block' => 'Blok toevoegen',
+		'Delete block' => 'Blok verwijderen',
+		'Move up' => 'Naar boven',
+		'Move down' => 'Naar beneden',
+		'Fill with images' => 'Vullen met afbeeldingen',
+    'No variants provided' => 'No variants provided',
+	];
