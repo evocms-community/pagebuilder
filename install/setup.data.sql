c1afe8022d980017d40fe508f63526c9e39a7056
Mikhail Noskov <nm@perm-tools.ru>
2017-08-28 17:43:29 +0000

    sql query for column 'container'

--- install/setup.data.sql
+++ install/setup.data.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS `{PREFIX}pagebuilder` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `document_id` int(10) unsigned NOT NULL,
-  `instance` varchar(255) DEFAULT NULL,
   `title` varchar(255) NOT NULL,
   `config` varchar(255) NOT NULL,
   `values` mediumtext NOT NULL,
@@ -9,4 +8,9 @@ CREATE TABLE IF NOT EXISTS `{PREFIX}pagebuilder` (
   PRIMARY KEY (`id`),
   KEY `document_id` (`document_id`,`instance`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+
+
+# Upgrading to v0.7.0 - adding column for sections container
+
+ALTER TABLE {PREFIX}pagebuilder ADD COLUMN container varchar(255) DEFAULT NULL AFTER document_id;
 
