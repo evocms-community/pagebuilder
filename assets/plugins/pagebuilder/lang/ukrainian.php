38d2a8f02b4e8ad9a02bfdd404b0e3aff09e11a2
Mikhail Noskov <nm@perm-tools.ru>
2018-12-20 04:19:32 +0000

    ukrainian lang added

---
+++ assets/plugins/pagebuilder/lang/ukrainian.php
@@ -0,0 +1,23 @@
+<?php
+
+	return [
+		'Add block' => 'Додати блок',
+		'Choose' => 'Вибрати',
+		'Insert element' => 'Вставити елемент',
+		'Delete element' => 'Видалити елемент',
+		'Select type of block' => 'Вибіреть тип блоку',
+		'Insert block' => 'Вставити блок',
+		'Delete block' => 'Видалити блок',
+		'Move up' => 'Перемістити наверх',
+		'Move down' => 'Перемістити вниз',
+		'Fill with images' => 'Заповнити зображеннями',
+		'Fill with files' => 'Заповнити файлами',
+		'No variants provided' => 'Немає варіантів вибору',
+		'No fields provided in this block' => 'У цьому блоці нема полів для заповнення',
+		'Visible' => 'Показувати',
+		'Show group items' => 'Показати (%s шт.)',
+		'Hide group items' => 'Приховати (%s шт.)',
+		'Export' => 'Експорт',
+		'Import' => 'Імпорт',
+	];
+
