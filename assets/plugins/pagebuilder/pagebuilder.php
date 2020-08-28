e68be651c0d888a258532e76c10ca759ffc415f5
2frags <0@2frags.com>
2020-08-28 16:56:36 +0300

    Variable width fields
    Use 'layout' => 'col-xx'
    from col-1 to col-12 to make inline fields

--- assets/plugins/pagebuilder/pagebuilder.php
+++ assets/plugins/pagebuilder/pagebuilder.php
@@ -739,7 +739,8 @@
                     ]));
                 }
 
-                case 'richtext': {
+                case 'richtext': {					
+					$params['layout'] = $field['layout'] ?? 'col-12';
                     if (isset($field['theme']) && !isset($this->themes[ $field['theme'] ]) && in_array($this->richeditor, [ 'TinyMCE4' ])) {
                         $result = $this->modx->invokeEvent('OnRichTextEditorInit', [
                             'editor'  => $this->richeditor,
@@ -766,7 +767,6 @@
                 case 'imageradio':
                 case 'radio': {
                     $params['layout'] = 'vertical';
-
                     if (isset($field['layout']) && in_array($field['layout'], [ 'horizontal', 'vertical' ])) {
                         $params['layout'] = $field['layout'];
                     }
@@ -779,6 +779,7 @@
                 }
 
                 default: {
+					$params['layout'] = $field['layout'] ?? 'col-12';
                     return $this->renderTpl('tpl/field_' . $field['type'] . '.tpl', $params) . $this->trigger('OnPBFieldRender', $params);
                 }
             }
